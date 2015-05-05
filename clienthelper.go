9b32ca09fce395192bc7433f48e8622eda5e6bb0
jason hu <sdhjl2000@gmail.com>
2015-05-05 16:23:56 +0800

    __

--- clienthelper.go
+++ clienthelper.go
@@ -10,7 +10,7 @@ func SendPostRequest(url string, data string) string {
 	var jsonStr = []byte(data)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
 	//req.Header.Set("X-Custom-Header", "myvalue")
-	//req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
